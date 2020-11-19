2970de8f5d04c47bdbb422e69ce53132522bd28d
sdumax <maxdiogu@gmail.com>
2020-11-19 09:38:20 +0100

    Folder structure & Info updates

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h4>DC Tech Inc🙌... WE MOVEEE😎</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
